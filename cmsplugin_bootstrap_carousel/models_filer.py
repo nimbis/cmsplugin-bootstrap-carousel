744803a36d396d3ab9e3cacfa9eb2d0204faf9b6
Eric Amador <eric.amador@nimbisservices.com>
2014-01-27 10:41:00 -0500

    Updated the models.py for the filer plugin. All new features should now fully support filer.

--- cmsplugin_bootstrap_carousel/models_filer.py
+++ cmsplugin_bootstrap_carousel/models_filer.py
@@ -7,18 +7,37 @@ from filer.fields.image import FilerImageField
 from cms.models.pluginmodel import CMSPlugin
 
 
-DEF_SIZE = (800, 600)
-        
 class Carousel(CMSPlugin):
     domid = models.CharField(max_length=50, verbose_name=_('Name'))
-    interval = models.IntegerField(default=5000)
-    
+    interval = models.IntegerField(default=5000
+                                   help_text="The amount of time in"
+                                   " milliseconds to delay cycling items."
+                                   " If zero carousel will not automatically"
+                                   " cycle.")
+
+    show_title = models.BooleanField(
+        help_text="Display image titles, if true.")
+
+    show_caption = models.BooleanField(
+        help_text="Display image captions, if true.",
+        default=True)
+
+    width = models.PositiveIntegerField(
+        _("width"),
+        help_text="Fixed width in pixels for carousel images.",
+        default=0)
+
+    height = models.PositiveIntegerField(
+        _("height"),
+        help_text="Fixed height in pixels for carousel images.",
+        default=0)
+
     def copy_relations(self, oldinstance):
         for item in oldinstance.carouselitem_set.all():
             item.pk = None
             item.carousel = self
             item.save()
-    
+
     def __unicode__(self):
         return self.domid
 
@@ -27,3 +46,4 @@ class CarouselItem(models.Model):
     caption_title = models.CharField(max_length=100, blank=True, null=True)
     caption_content = models.TextField(blank=True, null=True)
     image = FilerImageField(blank=True, null=True)
+    url = models.CharField(max_length=256, blank=True, default=None)
